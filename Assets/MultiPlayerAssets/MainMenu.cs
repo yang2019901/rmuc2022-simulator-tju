e98c6080c207c1ff18f97b62f96b3c152e4e773e
yang2019901 <iostream@tju.edu.cn>
2022-09-03 07:38:48 +0800

    add RMUC_UI namespace to prevent re-declaration

--- Assets/MultiPlayerAssets/MainMenu.cs
+++ Assets/MultiPlayerAssets/MainMenu.cs
@@ -5,97 +5,108 @@ using Mirror;
 using UnityEngine.UI;
 using TMPro;
 
-/* handle all UI-related events and call corresponding functions in other scripts */
-public class MainMenu : MonoBehaviour {
-    public BattleNetworkManager net_man;
-    public NetLobby net_lob;
+namespace RMUC_UI {
+    /* handle all UI-related events and call corresponding functions in other scripts */
+    public class MainMenu : MonoBehaviour {
+        public BattleNetworkManager net_man;
+        public NetLobby net_lob;
 
-    [Header("info")]
-    public GameObject Menu_player_info;
-    public TMP_InputField input_info;
-    [Header("mode")]
-    public GameObject Menu_player_mode;
-    [Header("option")]
-    public GameObject Menu_player_opt;
-    public TMP_InputField input_addr;
-    [Header("lobby")]
-    public GameObject Menu_player_lobby;
-    [Header("robot names")]
-    [Tooltip("set in Inspector and corresponding to avatar")]
-    public List<string> tags;
-    public List<AvatarTab> avatars;
+        [Header("info")]
+        public GameObject Menu_player_info;
+        public TMP_InputField input_info;
+        [Header("mode")]
+        public GameObject Menu_player_mode;
+        [Header("option")]
+        public GameObject Menu_player_opt;
+        public TMP_InputField input_addr;
+        [Header("lobby")]
+        public GameObject Menu_player_lobby;
+        [Header("robot names")]
+        [Tooltip("set in Inspector and corresponding to avatar")]
+        public List<string> ava_tags;
+        public List<AvatarTab> avatars;
 
-    void Start() {
-        /* set first menu to be player info menu */
-        SetPlayerInfo();
-    }
+        void Start() {
+            /* set first menu to be player info menu */
+            SetPlayerInfo();
+        }
 
-    /* start host -> go to lobby -> lobby behaviour -> start game */
-    public void SinglePlay() {
-        net_man.networkAddress = "localhost";
-        Debug.Log(net_man.networkAddress);
-        net_man.StartHost();
-        SetPlayerLobby();
-        /* single player => not allow to join */
-        net_lob.allow_join = false;
-        /* set owner to be local player */
-        net_lob.owner_connId = 0;
-    }
+        /* start host -> go to lobby -> lobby behaviour -> start game */
+        public void SinglePlay() {
+            net_man.networkAddress = "localhost";
+            Debug.Log(net_man.networkAddress);
+            net_man.StartHost();
+            SetPlayerLobby();
+            /* single player => not allow to join */
+            net_lob.allow_join = false;
+            /* set owner to be local player */
+            net_lob.owner_connId = 0;
+        }
 
-    public void JoinLobby() {
-        net_man.networkAddress = input_addr.text;
-        Debug.Log(net_man.networkAddress);
-        net_man.StartClient();
-        SetPlayerLobby();
-    }
+        public void JoinLobby() {
+            net_man.networkAddress = input_addr.text;
+            Debug.Log(net_man.networkAddress);
+            net_man.StartClient();
+            SetPlayerLobby();
+        }
 
-    public void CreateLobby() {
-        net_man.networkAddress = input_addr.text;
-        Debug.Log(net_man.networkAddress);
-        net_man.StartHost();
-        SetPlayerLobby();
-        /* muliplayer => allow to join */
-        net_lob.allow_join = true;
-        /* Host mode => local connId is 0 */
-        net_lob.owner_connId = 0;
-    }
+        public void CreateLobby() {
+            net_man.networkAddress = input_addr.text;
+            Debug.Log(net_man.networkAddress);
+            net_man.StartHost();
+            SetPlayerLobby();
+            /* muliplayer => allow to join */
+            net_lob.allow_join = true;
+            /* Host mode => local connId is 0 */
+            net_lob.owner_connId = 0;
+        }
 
-    public void Quit() {
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-#else
-    Application.Quit();
-#endif
-    }
+        public void Quit() {
+    #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+    #else
+        Application.Quit();
+    #endif
+        }
 
-    /// <summary> switch to another menu
-    public void SetPlayerInfo() {
-        DisableAllMenu();
-        Menu_player_info.SetActive(true);
-    }
+        public void TakeRobot(AvatarTab player_sync) {
+            Debug.Log("press button");
+            int idx = this.avatars.FindIndex(ava => ava==player_sync);
+            Debug.Log("cmd take robot");
+            NetLobby.AvatarMessage mes = new NetLobby.AvatarMessage(
+                this.ava_tags[idx], NetworkClient.connection.connectionId, input_info.text);
+            net_lob.ApplyAvatar(mes);
+        }
 
-    public void SetPlayerMode() {
-        DisableAllMenu();
-        Menu_player_mode.SetActive(true);
-    }
+        /// <summary> switch to another menu
+        public void SetPlayerInfo() {
+            DisableAllMenu();
+            Menu_player_info.SetActive(true);
+        }
 
-    public void SetPlayerOpt() {
-        DisableAllMenu();
-        Menu_player_opt.SetActive(true);
-    }
+        public void SetPlayerMode() {
+            DisableAllMenu();
+            Menu_player_mode.SetActive(true);
+        }
 
-    public void SetPlayerLobby() {
-        DisableAllMenu();
-        Menu_player_lobby.SetActive(true);
+        public void SetPlayerOpt() {
+            DisableAllMenu();
+            Menu_player_opt.SetActive(true);
+        }
+
+        public void SetPlayerLobby() {
+            DisableAllMenu();
+            Menu_player_lobby.SetActive(true);
+        }
+        /// <summary>
+        /// (wrap-up) set all menus to be inactive (obviously, it's invisible, too)
+        /// </summary>
+        void DisableAllMenu() {
+            Menu_player_mode.SetActive(false);
+            Menu_player_info.SetActive(false);
+            Menu_player_opt.SetActive(false);
+            Menu_player_lobby.SetActive(false);
+        }
+        /// </summary>
     }
-    /// <summary>
-    /// (wrap-up) set all menus to be inactive (obviously, it's invisible, too)
-    /// </summary>
-    void DisableAllMenu() {
-        Menu_player_mode.SetActive(false);
-        Menu_player_info.SetActive(false);
-        Menu_player_opt.SetActive(false);
-        Menu_player_lobby.SetActive(false);
-    }
-    /// </summary>
 }
--- Assets/MultiPlayerAssets/NetLobby.cs
+++ Assets/MultiPlayerAssets/NetLobby.cs
@@ -3,55 +3,83 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 
-public struct PlayerStateSync {
-    public int connId;          // connId of the player
-    public string player_name;  // the nickname that player inputs before enterring lobby
-    public bool owning_robot;   // whether taking a robot
-    public string robo_name;   // name of robot taken by the player, must be one of mainmenu.robo_names;
-    public bool ready;          // whether ready to start after taking robot
-}
-/// <summary>
-/// Orientation: define visual state and sync them; also do RPC
-/// </summary>
-public class NetLobby : NetworkBehaviour {
-    public readonly SyncList<PlayerStateSync> player_sync_all = new SyncList<PlayerStateSync>();
-    /* lobby owner */
-    [SyncVar]
-    public int owner_connId;
-    [SyncVar]
-    public bool allow_join;
-
-    public MainMenu mainmenu;
-
-    [Command]
-    public void CmdTakeRobot(string robot_s, int connId, string player_name) {
-        bool is_robot_taken = (-1 == player_sync_all.FindIndex(i => i.owning_robot && i.robo_name==robot_s));
-        if (is_robot_taken)
-            Debug.Log("server: the robot is taken!");
-        else {
-            /* make a new PlayerStateSync */
-            PlayerStateSync tmp = new PlayerStateSync();
-            tmp.connId = connId;
-            tmp.player_name = player_name;
-            tmp.owning_robot = true;
-            tmp.ready = true;
-            tmp.robo_name = robot_s;
-            /* find player info */
-            int idx = player_sync_all.FindIndex(i => i.connId == connId);
-            if (idx == -1)
-                player_sync_all.Add(tmp);
-            else
-                player_sync_all[idx] = tmp;
-        }
+namespace RMUC_UI {
+    public struct PlayerSync {
+        public int connId;          // connId of the player
+        public string player_name;  // the nickname that player inputs before enterring lobby
+        public bool owning_robot;   // whether taking a robot
+        public string robo_name;   // name of robot taken by the player, must be one of mainmenu.robo_names;
+        public bool ready;          // whether ready to start after taking robot
     }
+    /// <summary>
+    /// @Orientation: define visual state and sync them; also do RPC
+    /// @Style: Event/Call style
+    /// </summary>
+    public class NetLobby : NetworkBehaviour {
+        /* nested struct declaration => Netlobby.AvatarMessage instead of AvatarMessage */
+        public struct AvatarMessage : NetworkMessage {
+            public string robot_s;
+            public int connId;
+            public string player_name;
+            /* construct function with params */
+            public AvatarMessage(string robot_s, int connId, string player_name) {
+                this.robot_s = robot_s;
+                this.connId = connId;
+                this.player_name = player_name;
+            }
+        }
 
-    void Update() {
-        if (isClient) {
+        /// <summary>
+        /// Network Variables:
+        public readonly SyncList<PlayerSync> player_sync_all = new SyncList<PlayerSync>();
+        [SyncVar]
+        public int owner_connId; // lobby owner
+        [SyncVar]
+        public bool allow_join;
+        /// </summary> 
+
+
+        /// <summary>
+        /// Local Variables
+        public MainMenu mainmenu;
+        /// </summary>
+        
+        public void ApplyAvatar(AvatarMessage mes) {
+            bool is_avatar_taken = (-1 == player_sync_all.FindIndex(i => i.owning_robot && i.robo_name==mes.robot_s));
+            if (is_avatar_taken)
+                Debug.Log("server: the robot is taken!");
+            else {
+                /* make a new PlayerStateSync */
+                PlayerSync tmp = new PlayerSync();
+                tmp.connId = mes.connId;
+                tmp.player_name = mes.player_name;
+                tmp.owning_robot = true;
+                tmp.ready = true;
+                tmp.robo_name = mes.robot_s;
+                /* find player info */
+                int idx = player_sync_all.FindIndex(i => i.connId == mes.connId);
+                if (idx == -1)
+                    player_sync_all.Add(tmp);
+                else
+                    player_sync_all[idx] = tmp;
+            }
+        }
+
+        public override void OnStartClient() {
+            base.OnStartClient();
+            player_sync_all.Callback += OnPlayerSyncChanged;
+            /** ApplyAvatar => Action<AvatarMessage>, 
+                which tells me that usage of ApplyAvatar should be declared as ApplyAvatar(AvatarMessage mes) */
+            NetworkClient.RegisterHandler<AvatarMessage>(ApplyAvatar);
+        }
+
+        void OnPlayerSyncChanged(SyncList<PlayerSync>.Operation op, int index, PlayerSync oldval, PlayerSync newval) {
+            Debug.Log(string.Format("index: {}, oldval: {}, newval: {}", index, oldval, newval));
             /* go over player_sync_all, get every robot that has an owner and set mainmenu.robo_tag  */
             for (int i = 0; i < player_sync_all.Count; i++) {
                 if (player_sync_all[i].owning_robot) {
-                    int idx = mainmenu.tags.FindIndex(name => name==player_sync_all[i].robo_name);
-                    mainmenu.avatars[i].SetAvatar(player_sync_all[i]);
+                    int idx = mainmenu.ava_tags.FindIndex(name => name==player_sync_all[i].robo_name);
+                    mainmenu.avatars[i].SetAvatarTab(player_sync_all[i]);
                 }
             }
         }
